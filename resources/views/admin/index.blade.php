862f5989f1765f48e23ea04a1eebcfa20e037a19
Rahul <rahuldaibagna5955@gmail.com>
2024-08-12 20:39:41 +0530

    admin font d2d user font dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,7 +3,7 @@
     <div
         class="group-data-[sidebar-size=lg]:ltr:md:ml-vertical-menu group-data-[sidebar-size=lg]:rtl:md:mr-vertical-menu group-data-[sidebar-size=md]:ltr:ml-vertical-menu-md group-data-[sidebar-size=md]:rtl:mr-vertical-menu-md group-data-[sidebar-size=sm]:ltr:ml-vertical-menu-sm group-data-[sidebar-size=sm]:rtl:mr-vertical-menu-sm pt-[calc(theme('spacing.header')_*_1)] pb-[calc(theme('spacing.header')_*_0.8)] px-4 group-data-[navbar=bordered]:pt-[calc(theme('spacing.header')_*_1.3)] group-data-[navbar=hidden]:pt-0 group-data-[layout=horizontal]:mx-auto group-data-[layout=horizontal]:max-w-screen-2xl group-data-[layout=horizontal]:px-0 group-data-[layout=horizontal]:group-data-[sidebar-size=lg]:ltr:md:ml-auto group-data-[layout=horizontal]:group-data-[sidebar-size=lg]:rtl:md:mr-auto group-data-[layout=horizontal]:md:pt-[calc(theme('spacing.header')_*_1.6)] group-data-[layout=horizontal]:px-3 group-data-[layout=horizontal]:group-data-[navbar=hidden]:pt-[calc(theme('spacing.header')_*_0.9)]">
         <div class="container-fluid group-data-[content=boxed]:max-w-boxed mx-auto">
-            <div class="flex flex-col gap-2 py-4 md:flex-row md:items-center print:hidden">
+            {{-- <div class="flex flex-col gap-2 py-4 md:flex-row md:items-center print:hidden">
                 <div class="grow">
                     <h5 class="text-16">Ecommerce</h5>
                 </div>
@@ -14,7 +14,7 @@
                     </li>
                     <li class="text-slate-700 dark:text-zink-100">Ecommerce</li>
                 </ul>
-            </div>
+            </div> --}}
             <div class="grid grid-cols-12 2xl:grid-cols-12 gap-x-5">
                 <div class="relative col-span-12 overflow-hidden card 2xl:col-span-12 bg-slate-900">
                     <div class="absolute inset-0">
@@ -175,7 +175,7 @@
                         <div class="grid items-center grid-cols-12">
                             <div class="col-span-12 lg:col-span-8 2xl:col-span-7">
                                 <h5 class="mb-3 font-normal tracking-wide text-slate-200">
-                                    Welcome Paula Keenan 🎉
+                                    Welcome To CRM 🎉
                                 </h5>
                                 <p class="mb-5 text-slate-400 text-white">
                                     An ecommerce dashboard has just that purpose. It
