34e614b80f677fb8d2b44ed5459f0bc7baa99e3e
Rahul <rahuldaibagna5955@gmail.com>
2024-08-24 13:59:40 +0530

    ok

--- resources/views/admin/billing/index.blade.php
+++ resources/views/admin/billing/index.blade.php
@@ -161,7 +161,7 @@ padding: 5px 10px;
                                          disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800
                                           placeholder:text-slate-400 dark:placeholder:text-zink-200"
                                         placeholder="" id="agent_name" name="agent_name">
-                                    <input type="text"
+                                    <input type="hidden"
                                         class="form-input border-slate-200 dark:border-zink-500 focus:outline-none 
                                         focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 
                                         disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200
